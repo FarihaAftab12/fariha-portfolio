355e91d30a743c6042ad58149bbe207e7343a497
Fariha Aftab <farihaaftab0000@gmail.com>
2026-06-08 23:18:17 +0500

    fixed avatar not loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+'@ | Set-Content -Encoding UTF8 next.config.ts';
